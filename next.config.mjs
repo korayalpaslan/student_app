bacae3206c8363dd2ae4b809aa6b5345215dab86
Koray Alpaslan <alpaslankoray@hotmail.com>
2025-12-29 14:49:40 +0300

    vercel migrate

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    API_URL: "https://studenttrackingapp.netlify.app",
+    API_URL: "https://stapp.vercel.app",
     // API_URL: "http://localhost:3000",
   },
 };
